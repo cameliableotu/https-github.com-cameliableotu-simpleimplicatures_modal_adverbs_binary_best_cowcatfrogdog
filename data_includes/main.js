ef3fb897031b9fdcb88c40850b9a3bbc7341041b
cameliableotu <53470693+cameliableotu@users.noreply.github.com>
2019-09-10 11:15:30 +0200

    Update main.js

--- data_includes/main.js
+++ data_includes/main.js
@@ -2205,7 +2205,7 @@ PennController("experiment" ,
 	        .print(),
 	       newText ("<p> Very well. Yes, the shadow belongs to a dog indeed. It could be one of the three dogs you saw in the beginning.</p>")
 	         ,         
-	     newImage ("threedogs1.png")
+	     newImage ("threedogs.png")
                .print ()
 	       ,
 	       newKey(" ")
